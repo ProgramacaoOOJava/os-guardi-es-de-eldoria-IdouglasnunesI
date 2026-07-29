8666ab9bd6447dc8a2ae786f2c4809a69c211582
IdouglasnunesI <douglasdougnunes@gmail.com>
2026-07-28 22:30:34 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,19 @@
 public class Personagem {
+
+    // Atributos do personagem
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
-        this.nome = nome;
-        .
-        .
-        .
-
-    }
-
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+    // Método para exibir as informações do personagem
+    void exibirStatus() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
+        System.out.println("---------------------------");
     }
 }
-
