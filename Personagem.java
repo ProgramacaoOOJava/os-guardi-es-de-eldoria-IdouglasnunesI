2adcd5107fef8f8cfc74aa0c5d0f4710057e17d2
IdouglasnunesI <douglasdougnunes@gmail.com>
2026-07-28 22:56:33 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -1,19 +1,29 @@
-public class Personagem {
+public abstract class Personagem {
 
-    // Atributos do personagem
-    String nome;
-    String classe;
-    int nivel;
-    int pontosDeVida;
-    double poderBase;
+    protected String nome;
+    protected String classe;
+    protected int nivel;
+    protected int pontosDeVida;
+    protected double poderBase;
 
-    // Método para exibir as informações do personagem
-    void exibirStatus() {
+    // Construtor da superclasse
+    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
+    }
+
+    // Método comum para todas as subclasses
+    public void exibirStatus() {
         System.out.println("Nome: " + nome);
         System.out.println("Classe: " + classe);
         System.out.println("Nível: " + nivel);
         System.out.println("Pontos de Vida: " + pontosDeVida);
         System.out.println("Poder Base: " + poderBase);
-        System.out.println("---------------------------");
     }
+
+    // Método abstrato que será implementado nas subclasses
+    public abstract void usarHabilidadeEspecial();
 }
